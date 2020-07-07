ffa1b830afcb0ce9b0861ae9b4d98f2ed1512c04
alex-diachenko <dyachenko664@gmail.com>
2020-07-07 16:07:42 +0300

    changed sendgrid to nodemailer

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -4,18 +4,22 @@ import { ApplicationError } from "common";
 import { HashEncrypter } from "common/hash-encrypter";
 import { model } from "mongoose";
 import { UserSchema } from "schemas/user.schema";
-import { api_key } from "environment/environment";
-
 export const UserModel = model("users", UserSchema);
-const sgMail = require("@sendgrid/mail");
-sgMail.setApiKey(api_key);
-
+const nodemailer = require('nodemailer');
+const smtpTransport = require('nodemailer-smtp-transport');
+const transporter = nodemailer.createTransport(smtpTransport({
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  auth: {
+    user: 'testspotball@gmail.com',
+    pass: 'alex123456789!'
+  }
+}));
 @injectable()
 export class AuthService {
   constructor(
     @inject(HashEncrypter) private _hashEncrypter: HashEncrypter,
   ) {}
-
   async login(email: string, password: string): Promise<AuthUserModel> {
     const hashedPassword = this._hashEncrypter.getHash(password);
     const user: any = await UserModel.findOne({
@@ -27,52 +31,53 @@ export class AuthService {
     }
     return user;
   }
-
   async findOneUserByEmail(email): Promise<any> {
     return UserModel.findOne({
       email: email,
     });
   }
-
   async addNewUser(user): Promise<any> {
     try {
       const unhashedPassword = user.password;
       const hashedPassword = this._hashEncrypter.getHash(user.password);
       user.password = hashedPassword;
       let userData = Object.assign({ _id: null }, user);
-      const data: any = await UserModel.create(userData);
-
-      const msg2 = {
+      const mailOptions = {
+        from: 'testspotball@gmail.com',
         to: `${user.email}`,
-        from: "develop@webcentriq.com",
         subject: "Spot That Ball support team",
         text: "Your registration credentials",
         html: `
         <html>
-        <head>
-          <title></title>
-        </head>
-        <body>
-          <div data-role="module-unsubscribe" class="module" role="module" data-type="unsubscribe" style="background: #33cc8e1a; color:#444444; font-size:12px; line-height:20px; padding:16px 16px 16px 16px; text-align:Center;" data-muid="4e838cf3-9892-4a6d-94d6-170e474d21e5">
-            <div class="Unsubscribe--addressLine">
-              <p class="Unsubscribe--senderName"
-                style="font-size:18px;line-height:20px"
-              >
-                Sent by: Spot that ball
-              </p>
-              <p class="Unsubscribe--senderName"
-                style="font-size:14px;line-height:20px"
-              >
-                You are successfully registered to game your password is - ${unhashedPassword}, your login is ${user.email}
-              </p>
+          <head>
+            <title></title>
+          </head>
+          <body>
+            <div data-role="module-unsubscribe" class="module" role="module" data-type="unsubscribe" style="background: #33cc8e1a; color:#444444; font-size:12px; line-height:20px; padding:16px 16px 16px 16px; text-align:Center;" data-muid="4e838cf3-9892-4a6d-94d6-170e474d21e5">
+              <div class="Unsubscribe--addressLine">
+                <p class="Unsubscribe--senderName"
+                  style="font-size:18px;line-height:20px"
+                >
+                  Sent by: Spot that ball
+                </p>
+                <p class="Unsubscribe--senderName"
+                  style="font-size:14px;line-height:20px"
+                >
+                  You are successfully registered to game your password is - ${unhashedPassword}, your login is ${user.email}.
+                  Use it here http://stb.webcentriq.com/#/login
+                </p>
+              </div>
             </div>
-          </div>
-        </body>
-      </html>
-        `,
+          </body>
+        </html>
+        `
       };
-      sgMail.send(msg2);
-
+      await transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          return;
+        }
+      });
+      const data: any = await UserModel.create(userData);
       return data;
     } catch (err) {
       console.log(err.response.body);
